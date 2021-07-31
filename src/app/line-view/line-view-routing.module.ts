4bdada58232b53291759186b9075c840ca5c21ef
Neha <nhlodha7@gmail.com>
2021-07-31 15:54:10 +0530

    Release Notes : Date format change

--- src/app/line-view/line-view-routing.module.ts
+++ src/app/line-view/line-view-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
       {
         path: '',
         loadChildren: () => import('../manuals/manuals.module').then((m) => m.ManualsModule),
-        data: { breadcrumb: 'Masters' },
+        data: { breadcrumb: 'manuals' },
       },
     ]
   }
